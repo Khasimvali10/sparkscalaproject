4f78b73f49b41306b22d322e85c74c77128ac9fd
unknown <khasimvali10@gmail.com>
2021-08-30 20:35:52 +0530

    My first commit

---
+++ src/main/scala/org/example/demoexample.scala
@@ -0,0 +1,17 @@
+package org.example
+
+import org.apache.spark.sql.SparkSession
+
+object demoexample {
+  def main (args:Array[String]): Unit =
+  {
+    val sc = SparkSession.builder().appName("demo").master("local").getOrCreate()
+    sc.sparkContext.setLogLevel("Error")
+    val rdd = sc.sparkContext.parallelize(Array(1,2,3,4,5,6,7,8,9,10))
+    rdd.collect().foreach(println)
+    val rdd2 = rdd.filter(a=>a>6)
+    rdd2.collect().foreach(println)
+
+  }
+
+}
